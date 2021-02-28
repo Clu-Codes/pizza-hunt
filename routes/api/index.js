64c53c614e966842959da9ce46f4f33d579142fc
Clu <retsulcconnor@gmail.com>
2021-02-28 15:43:02 -0500

    fixed module.exports issue, test CRUD functionality, wrote fetch request to post pizza recipes from /add-pizzas/ page

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const pizzaRoutes = ('./pizza-routes');
+const pizzaRoutes = require('./pizza-routes');
 
 // add prefix of '/pizzas' to routes created in 'pizza-routes'
 router.use('/pizzas', pizzaRoutes);
